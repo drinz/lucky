47cb87c45677660f82c9d0cddd964534e7d4db70
drinz <86057200+drinz@users.noreply.github.com>
2021-07-14 06:32:37 +0700

    Create Lucky.py

---
+++ Lucky.py
@@ -0,0 +1,12 @@
+#Fun
+#hahaaa
+#Notbad
+#Farez
+import random
+import time
+
+lucky = input("Want to see lucky numbers?")
+time.sleep(0.5)
+print("see your lucky number")
+time.sleep(2.5)
+print("your lucky number:",random.randint(0,1001))
